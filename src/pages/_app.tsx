993d95abdee12d16c2ce8dd0226ccfd9f3527036
Kaspar Kallas <kaspar@superfluid.finance>
2022-02-05 16:53:56 +0200

    Disable SSR for main content of pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,20 @@
-import * as React from 'react';
-import Head from 'next/head';
-import { AppProps } from 'next/app';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import createEmotionCache from '../utils/createEmotionCache';
+import * as React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "../utils/createEmotionCache";
 import { wrapper } from "../redux/store";
 import SfAppBar from "../components/SfAppBar";
 import { FC, useEffect, useState } from "react";
 import Box from "@mui/material/Box";
-import "../styles/graphiql.min.css"
-import "../styles/app.css"
+import "../styles/graphiql.min.css";
+import "../styles/app.css";
 import { ThemeProvider } from "@mui/material/styles";
 import useSfTheme from "../styles/useSfTheme";
 import { hotjar } from "react-hotjar";
-import Footer from '../components/Footer';
+import Footer from "../components/Footer";
+import dynamic from "next/dynamic";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -28,11 +29,14 @@ function MyApp(props: MyAppProps) {
 
   useEffect(() => {
     if (process.env.NEXT_PUBLIC_HJID && process.env.NEXT_PUBLIC_HJSV) {
-      hotjar.initialize(Number(process.env.NEXT_PUBLIC_HJID), Number(process.env.NEXT_PUBLIC_HJSV))
+      hotjar.initialize(
+        Number(process.env.NEXT_PUBLIC_HJID),
+        Number(process.env.NEXT_PUBLIC_HJSV)
+      );
     } else {
-      console.log("Hotjar not initialized.")
+      console.log("Hotjar not initialized.");
     }
-  })
+  });
 
   return (
     <CacheProvider value={emotionCache}>
@@ -46,14 +50,18 @@ function MyApp(props: MyAppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Box sx={{
-          display: "flex",
-          flexFlow: "column",
-          height: "100vh"
-        }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexFlow: "column",
+            height: "100vh",
+          }}
+        >
           <SfAppBar />
           <Box component="main" sx={{ height: "100vh" }}>
-            <Component {...pageProps} />
+            <NoSsr>
+              <Component {...pageProps} />
+            </NoSsr>
           </Box>
           <Footer />
         </Box>
@@ -71,9 +79,13 @@ export const getRenderedThemeMode = (): "light" | "dark" => {
   const themeModeMetaElement = window.document.getElementById("theme-mode");
   if (!themeModeMetaElement) {
     console.error("Meta tag for theme mode not found!"); // Sometimes happens with HMR.
-    return 'light'; // *sigh*, default to light.
+    return "light"; // *sigh*, default to light.
   }
-  return themeModeMetaElement.getAttribute('content') as "light" | "dark";
+  return themeModeMetaElement.getAttribute("content") as "light" | "dark";
 };
 
+const Noop: FC = ({ children }) => {
+  return <>{children}</>;
+};
 
+const NoSsr = dynamic(() => Promise.resolve(Noop), { ssr: false });
