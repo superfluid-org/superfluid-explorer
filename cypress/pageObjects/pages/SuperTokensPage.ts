952315d47a081b902653c0cd16a83e524a3f2a09
Mariam Tavadze <mariam.tavadze@testdevlab.com>
2022-05-02 11:38:46 +0400

    updated super tokens page cases

--- cypress/pageObjects/pages/SuperTokensPage.ts
+++ cypress/pageObjects/pages/SuperTokensPage.ts
@@ -2,7 +2,7 @@ import {BasePage} from "../BasePage";
 
 const TOKENS_BUTTON = 'a[href="/super-tokens"]'
 const NETWORK_RIGHT_ARROW = "[data-testid=KeyboardArrowRightIcon]"
-const LOADING_SPINNER = ".svg.MuiCircularProgress-svg.css-1idz92c-MuiCircularProgress-svg"
+const LOADING_SPINNER = ".MuiCircularProgress-svg"
 const SUPER_TOKENS_NAME = "[data-cy=token-name] > *"
 const SUPER_TOKENS_SYMBOL = "[data-cy=token-symbol] span"
 const SUPER_TOKENS_ADDRESS = "[data-cy=token-address]"
@@ -18,6 +18,7 @@ const CHIP_NAME = "[data-cy=chip-name]"
 const CHIP_SYMBOL ="[data-cy=chip-symbol]"
 const CHIP_LISTED_STATUS = "[data-cy=chip-listed-status]"
 const FILTERING_CHIP = ".css-nj19ab-MuiStack-root > .MuiButtonBase-root"
+const CANCEL_ICON = "[data-testid=CancelIcon]"
 
 export class SuperTokensPage extends BasePage {
 
@@ -26,22 +27,24 @@ export class SuperTokensPage extends BasePage {
   }
 
   static switchLNetworkAndValidateTokens(network: string) {
+    this.isNotVisible(LOADING_SPINNER)
     this.click(NETWORK_RIGHT_ARROW)
     this.click("[data-cy=" + network + "-landing-button]")
     this.hasAttributeWithValue("[data-cy=" + network + "-landing-button]", "aria-selected", "true")
-    this.doesNotExist(LOADING_SPINNER)
+    this.isNotVisible(LOADING_SPINNER)
     this.hasLength(SUPER_TOKENS_NAME, 10)
     this.hasLength(SUPER_TOKENS_SYMBOL, 10)
     this.hasLength(SUPER_TOKENS_ADDRESS, 10)
   }
 
   static filterByTokenName(network: string){
+   // cy.get(SUPER_TOKENS_NAME).invoke("text".as("tokensName")
     cy.fixture("supertokensData").then(supertokens => {
       this.click(FILTER_BUTTON)
       this.type(TOKEN_NAME_FILTER, supertokens[network].TokenName)
       this.hasText(CHIP_NAME,supertokens[network].TokenName)
       this.click(FILTER_CLOSE_BUTTON)
-      this.doesNotExist(LOADING_SPINNER)
+      cy.wait(1000) //It's the only way I was able to make it work
       cy.get(SUPER_TOKENS_NAME).each((
         $el) => {
         expect($el.text()).contains(supertokens[network].TokenName)
@@ -50,12 +53,13 @@ export class SuperTokensPage extends BasePage {
   }
 
   static filterByTokenSymbol(network: string){
+   // cy.get(SUPER_TOKENS_SYMBOL).as("tokensSymbol")
     cy.fixture("supertokensData").then(supertokens => {
       this.click(FILTER_BUTTON)
       this.type(TOKEN_SYMBOL_FILTER, supertokens[network].TokenSymbol)
       this.hasText(CHIP_SYMBOL,supertokens[network].TokenSymbol)
       this.click(FILTER_CLOSE_BUTTON)
-      this.isNotVisible(LOADING_SPINNER)
+      cy.wait(1000)
       cy.get(SUPER_TOKENS_SYMBOL).each((
         $el) => {
         expect($el.text()).contains(supertokens[network].TokenSymbol)
@@ -64,36 +68,57 @@ export class SuperTokensPage extends BasePage {
   }
 
   static filterByListed(){
+    //cy.get(LISTED_TOKEN_ROW).as("listedToken")
     this.click(FILTER_BUTTON)
     this.click(FILTER_LISTED_YES_BUTTON)
     this.hasText(CHIP_LISTED_STATUS,"Yes")
     this.click(FILTER_CLOSE_BUTTON)
-    this.isNotVisible(LOADING_SPINNER)
+    cy.wait(1000)
     cy.get(LISTED_TOKEN_ROW).each((
       $el) => {
       expect($el.text()).to.be.equal("Yes")
-    })
+   })
   }
 
   static filterByNotListed() {
+   // cy.get(LISTED_TOKEN_ROW).as("listedToken")
     this.click(FILTER_BUTTON)
     this.click(FILTER_LISTED_NO_BUTTON)
     this.hasText(CHIP_LISTED_STATUS,"No")
     this.click(FILTER_CLOSE_BUTTON)
-    this.isNotVisible(LOADING_SPINNER)
+    cy.wait(1000)
     cy.get(LISTED_TOKEN_ROW).each((
       $el) => {
       expect($el.text()).to.be.equal("No")
     })
   }
 
-  // @mariam Leave this as a seperate step to close the filter, but
-  //When starting the filtering save the table data as an alias
-  //Then after finishing the filtering, compare the table data with the alias in a new step
-  static resetFilter(){
+  static resetFilter() {
     this.click(FILTER_BUTTON)
     this.click(FILTER_RESET_BUTTON)
     this.doesNotExist(FILTERING_CHIP)
-
   }
+
+  //When starting the filtering save the table data as an alias
+  //Then after finishing the filtering, compare the table data with the alias in a new step
+
+  //not sure how to do it
+  static resetNameFilter() {
+    this.click(CANCEL_ICON)
+    this.doesNotExist(FILTERING_CHIP)
+    cy.get("@tokensName").should("contain", "")
+  }
+
+  static resetSymbolFilter(){
+    this.click(CANCEL_ICON)
+    this.doesNotExist(FILTERING_CHIP)
+    cy.get("@tokensSymbol").should("contain", "")
+  }
+
+  static resetListedFilter(){
+    this.click(CANCEL_ICON)
+    this.doesNotExist(FILTERING_CHIP)
+    cy.get("@listedToken").should("contain", "")
+  }
+
 }
