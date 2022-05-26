1b63e002eb4fcc9f4e72c20a6a82cd942475866c
elvijsTDL <elvijs.dzirkals@testdevlab.com>
2022-05-26 13:26:51 +0300

    Fix account page test case

--- cypress/pageObjects/pages/AccountPage.ts
+++ cypress/pageObjects/pages/AccountPage.ts
@@ -220,7 +220,8 @@ export class AccountPage extends BasePage {
     cy.fixture("toolTipStringsAndLinks").then(fixture => {
       cy.get("[data-cy=" + tooltip + "-tooltip]").trigger("mouseover").then(el => {
         cy.wrap(el).should("have.attr", "aria-labelledby").then(attr => {
-          this.hasText("#" + attr, fixture[tooltip + "-tooltip"])
+          console.log(attr)
+          this.hasText(`[id="${attr}"]`, fixture[tooltip + "-tooltip"])
           this.hasAttributeWithValue("[data-cy=" + tooltip + "-tooltip-link]", "href", fixture[tooltip + "-tooltip-link"])
         })
       })
