9bcf538ccb01544a8c9d460694142c281558b908
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-06-15 23:57:38 +0100

    isListed label fixed + dont persist filter state for default

--- src/components/Tables/SuperTokensTable.tsx
+++ src/components/Tables/SuperTokensTable.tsx
@@ -65,11 +65,24 @@ const SuperTokensTable: FC<SuperTokensTableProps> = ({ network }) => {
 
   const [listedStatus, setListedStatus] = useState<ListedStatus | null>(null);
 
-  const setUrlQueryParam = (paramObject: RequiredTokensQuery) => {
-    const url = new URL(window.location.href);
-    url.searchParams.set("filter", JSON.stringify(paramObject))
+  const defaultFilter: Token_Filter = {
+    isSuperToken: true,
+  };
 
-    window.history.replaceState({}, "", url.toString());
+  const defaultArg: RequiredTokensQuery = {
+    chainId: network.chainId,
+    filter: defaultFilter,
+    pagination: defaultPaging,
+    order: defaultOrdering,
+  }
+
+  const setUrlQueryParam = (paramObject: RequiredTokensQuery) => {
+    // Dont set the url if the query is the same as the default
+    if(paramObject != defaultArg){
+      const url = new URL(window.location.href);
+      url.searchParams.set("filter", JSON.stringify(paramObject))
+      window.history.replaceState({}, "", url.toString());
+    }
   };
 
   const resetUrl = () => {
@@ -77,29 +90,16 @@ const SuperTokensTable: FC<SuperTokensTableProps> = ({ network }) => {
     window.history.replaceState({}, "", url.toString());
   };
 
-  const defaultFilter: Token_Filter = {
-    isSuperToken: true,
-  };
-
   const urlQueryParams = new URLSearchParams(window.location.search);
 
   const createDefaultArg = (): RequiredTokensQuery => {
-    const filter = {
-      chainId: network.chainId,
-      filter: defaultFilter,
-      pagination: defaultPaging,
-      order: defaultOrdering,
-    }
+
     const urlFilter = urlQueryParams.get("filter");
     if(urlFilter != null) {
       const urlFilterObject: RequiredTokensQuery = JSON.parse(urlFilter);
-      if(typeof urlFilterObject.filter.isListed === "boolean") {
-        let status = ListedStatus.Listed;
-        if(urlFilterObject.filter.isListed === false) status = ListedStatus.NotListed;
+      return urlFilterObject
       }
-      return urlFilterObject;
-    }
-    return filter;
+    return defaultArg;
   }
 
   const [queryArg, setQueryArg] = useState<RequiredTokensQuery>(
@@ -124,7 +124,12 @@ const SuperTokensTable: FC<SuperTokensTableProps> = ({ network }) => {
   };
 
   useEffect(() => {
-    onQueryArgsChanged(createDefaultArg());
+    const defaultArgs = createDefaultArg();
+    if (defaultArgs.filter.isListed != null) {
+      const listedStatus = defaultArgs.filter.isListed ? ListedStatus.Listed : ListedStatus.NotListed;
+      setListedStatus(listedStatus);
+    }
+    onQueryArgsChanged(defaultArgs);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [network]);
 
