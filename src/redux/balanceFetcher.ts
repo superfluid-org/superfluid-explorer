2edeb541212f45921f3d8ad3ca8975616ccd6c8c
Daniel <tokdaniel4@gmail.com>
2022-10-04 14:20:25 +0200

    refactored createFetching

--- src/redux/balanceFetcher.ts
+++ src/redux/balanceFetcher.ts
@@ -1,6 +1,5 @@
 import { providers } from "@0xsequence/multicall";
 import { getFramework } from "@superfluid-finance/sdk-redux";
-import { ethers } from "ethers";
 import { networks } from "./networks";
 
 const multicallContractAddress = "0xcA11bde05977b3631167028862bE2a173976CA11";
@@ -9,7 +8,6 @@ export type RealtimeBalance = {
   balance: string;
   balanceTimestamp: number;
   flowRate: string;
-  e;
 };
 
 export type BalanceQueryParams = {
@@ -36,50 +34,38 @@ const mutableNetworkStates: Record<
  */
 const createFetching = async (chainId: number) => {
   const state = mutableNetworkStates[chainId];
-  state.nextFetching = null;
+  const queries = state.queryBatch;
+  const framework = await getFramework(chainId);
 
-  if (state.queryBatch.length) {
-    const queries = state.queryBatch.splice(0, state.queryBatch.length); // Makes a copy of the queries and empties the original array.
-    const framework = await getFramework(chainId);
+  const provider = new providers.MulticallProvider(framework.settings.provider);
 
-    const provider = new providers.MulticallProvider(
-      framework.settings.provider
-    );
-
-    const results = await Promise.all(
-      queries
-        .filter((x) => x.isSuperToken)
-        .map((x) =>
-          Promise.all([
-            x.params,
-            framework.cfaV1.getNetFlow({
-              account: x.params.accountAddress,
-              superToken: x.params.tokenAddress,
-              providerOrSigner: provider,
-            }),
-            provider.getBalance(x.params.tokenAddress),
-          ])
-        )
-    );
-
-    const mappedResult: Record<string, RealtimeBalance> = Object.fromEntries(
-      results.map(([params, netFlow, realtimeBalanceOfNow]) => {
-        console.log(params, netFlow, realtimeBalanceOfNow);
-        return [
+  const results = await Promise.all(
+    queries
+      .filter(({ isSuperToken }) => isSuperToken)
+      .map(({ params }) =>
+        Promise.all([
           getKey(params),
-          {
-            balance: ethers.BigNumber.from(realtimeBalanceOfNow).toString(),
-            balanceTimestamp: ethers.BigNumber.from("0").toNumber(),
-            flowRate: ethers.BigNumber.from(netFlow).toString(),
-          } as RealtimeBalance,
-        ];
-      })
-    );
+          provider.getBalance(params.tokenAddress),
+          framework.cfaV1.getNetFlow({
+            superToken: params.tokenAddress,
+            account: params.accountAddress,
+            providerOrSigner: provider,
+          }),
+        ])
+      )
+  );
 
-    return mappedResult;
-  }
-
-  return {};
+  const mappedResults: Record<string, RealtimeBalance> = Object.fromEntries(
+    results.map(([key, balance, flowRate]) => [
+      key,
+      {
+        balance: balance.toString(),
+        balanceTimestamp: 0,
+        flowRate,
+      },
+    ])
+  );
+  return mappedResults;
 };
 
 export const balanceFetcher = {
