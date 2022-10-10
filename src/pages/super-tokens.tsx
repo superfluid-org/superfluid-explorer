39014f9052f999dca4ee145203feeedb358896a8
didi <git@d10r.net>
2022-10-10 20:57:33 +0200

    add handler for /supertokens and declare /super-tokens legacy

--- src/pages/super-tokens.tsx
+++ src/pages/super-tokens.tsx
@@ -2,11 +2,12 @@ import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-const SuperTokensRedirect: NextPage = () => {
+// redirects the legacy url /super-tokens to the default network specific page
+const SuperTokensLegacyRedirect: NextPage = () => {
     const router = useRouter();
     useEffect(() => void router.replace("/matic/supertokens") ,[]);
 
     return null;
 };
 
-export default SuperTokensRedirect;
+export default SuperTokensLegacyRedirect;
