723845f4c04b6b405f96181fc7c4c6c506815e56
sky cherfa <sherifcherfa@gmail.com>
2022-11-19 08:10:00 -0500

    create query and place the account address in node

--- src/components/Visualisers/Map.tsx
+++ src/components/Visualisers/Map.tsx
@@ -1,8 +1,5 @@
 import { useCallback, FC, ReactElement } from 'react';
-
-import { Network } from "../../redux/networks";
 import ReactFlow, {
-  MiniMap,
   Controls,
   Background,
   useNodesState,
@@ -11,22 +8,93 @@ import ReactFlow, {
 } from 'reactflow';
 // 👇 you need to import the reactflow styles
 import 'reactflow/dist/style.css';
+import {
+  createSkipPaging,
+  Ordering,
+  Stream_OrderBy,
+} from "@superfluid-finance/sdk-core";
+import { StreamsQuery } from "@superfluid-finance/sdk-redux";
+import isEqual from "lodash/isEqual";
+import {
+  useEffect,
+  useState,
+} from "react";
+import useDebounce from '../../hooks/useDebounce';
+import { Network } from "../../redux/networks";
+import { sfSubgraph } from "../../redux/store";
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
+type RequiredStreamQuery = Required<Omit<StreamsQuery, "block">>;
 
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+export const incomingStreamOrderingDefault: Ordering<Stream_OrderBy> = {
+  orderBy: "updatedAtTimestamp",
+  orderDirection: "desc",
+};
+
+export const incomingStreamPagingDefault = createSkipPaging({
+  take: 10,
+});
+
+export enum StreamStatus {
+  Active,
+  Inactive,
+}
 
 const Map: FC<{
   network: Network;
   accountAddress: string;
 }> = ({network, accountAddress}): ReactElement => {
+
+
+const initialNodes = [
+  { id: accountAddress, position: { x: 500, y: 200 }, data: { label: accountAddress } },
+];
+
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
   const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+
+  const defaultFilter = {
+    receiver: accountAddress,
+  };
+
+  const createDefaultArg = (): RequiredStreamQuery => ({
+    chainId: network.chainId,
+    filter: defaultFilter,
+    pagination: incomingStreamPagingDefault,
+    order: incomingStreamOrderingDefault,
+  });
+
+  const [streamsQueryArg, setStreamsQueryArg] = useState<RequiredStreamQuery>(createDefaultArg());
+
+  const [streamsQueryTrigger, streamsQueryResult] =
+    sfSubgraph.useLazyStreamsQuery();
+
+  const streamsQueryTriggerDebounced = useDebounce(streamsQueryTrigger, 250);
+
+  const onStreamQueryArgsChanged = (newArgs: RequiredStreamQuery) => {
+    setStreamsQueryArg(newArgs);
+
+    if (
+      streamsQueryResult.originalArgs &&
+      !isEqual(streamsQueryResult.originalArgs.filter, newArgs.filter)
+    ) {
+      streamsQueryTriggerDebounced(newArgs, true);
+    } else {
+      streamsQueryTrigger(newArgs, true);
+    }
+  };
+
+  useEffect(() => {
+    onStreamQueryArgsChanged(createDefaultArg());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [network, accountAddress]);
+
+  const tableRows = streamsQueryResult.data?.data || [];
+
+  console.log('look here', tableRows);
 
   return (
     <ReactFlow
@@ -41,7 +109,6 @@ const Map: FC<{
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
     >
-      <MiniMap />
       <Controls />
       <Background />
     </ReactFlow>
