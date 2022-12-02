b5158057e4b16ddff4e66abc86485e82ae5bd7fa
Kaspar Kallas <kaspar@superfluid.finance>
2022-12-02 11:46:48 +0200

    Update SDK's and Subgraph endpoint

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -48,11 +48,7 @@ const infuraProviders = networks.map((network) => ({
       chainId: network.chainId,
       provider: new providers.MulticallProvider(
         new ethers.providers.StaticJsonRpcProvider(network.rpcUrl)
-      ),
-      customSubgraphQueriesEndpoint:
-        network.chainId === 100 && new Date() < new Date(2022, 10, 4, 12)
-          ? "https://subgraph.satsuma-prod.com/superfluid/xdai/api"
-          : undefined,
+      )
     }),
 }));
 
