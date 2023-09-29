4c8e6d6627caae7585dff2ce1fcfe80468ebc722
Kaspar Kallas <kaspar@superfluid.finance>
2023-09-29 14:09:44 +0000

    fix depletion date too far in the future causing app crash

--- src/components/DepletionDate.tsx
+++ src/components/DepletionDate.tsx
@@ -8,41 +8,46 @@ import React from 'react';
 import { BigNumber } from "ethers";
 
 interface DepletionDateProps {
-  balance: string;
-  balanceTimestamp: number;
-  flowRate: string;
-  tooltipProps?: TooltipProps;
+    balance: string;
+    balanceTimestamp: number;
+    flowRate: string;
+    tooltipProps?: TooltipProps;
 }
 
+const MAX_DATE_NUMBER = BigNumber.from(8640000000000000);
+
 const DepletionDate: React.FC<DepletionDateProps> = ({ balance, balanceTimestamp, flowRate, tooltipProps }) => {
-  const flowRateNum = parseFloat(flowRate);
+    const flowRateNum = parseFloat(flowRate);
 
-  if (flowRateNum < 0) {
-    const balanceBigNumber = BigNumber.from(balance);
-    const depletionTime = balanceBigNumber.div(BigNumber.from(Math.abs(flowRateNum).toString()));
-    const depletionDate = new Date(balanceTimestamp * 1000 + depletionTime.toNumber() * 1000);
-    const depletionDateString = `${depletionDate.toLocaleDateString()} ${depletionDate.toLocaleTimeString()}`;
-    const unixTimestamp = Math.floor(depletionDate.getTime() / 1000);
+    if (flowRateNum < 0) {
+        const balanceBigNumber = BigNumber.from(balance);
+        const depletionTime = balanceBigNumber.div(BigNumber.from(Math.abs(flowRateNum)));
 
-    return (
-        <>
-            <Box display="inline">
-                <Tooltip
-                    title={
-                        <React.Fragment>
-                            <span>Timestamp: {unixTimestamp}</span>
-                        </React.Fragment>
-                    }
-                    {...tooltipProps}
-                    placement="right" arrow>
-                    <span>{depletionDateString}</span>
-                </Tooltip>
-            </Box>
-        </>
-      );
-  }
+        const depletionTimestamp = BigNumber.from(balanceTimestamp).mul(1000).add(depletionTime).mul(1000);
 
-  return null;
+        const depletionDate = new Date(depletionTimestamp.gt(MAX_DATE_NUMBER) ? depletionTimestamp.toNumber() : MAX_DATE_NUMBER.toNumber());
+        const depletionDateString = `${depletionDate.toLocaleDateString()} ${depletionDate.toLocaleTimeString()}`;
+        const unixTimestamp = Math.floor(depletionDate.getTime() / 1000);
+
+        return (
+            <>
+                <Box display="inline">
+                    <Tooltip
+                        title={
+                            <React.Fragment>
+                                <span>Timestamp: {unixTimestamp}</span>
+                            </React.Fragment>
+                        }
+                        {...tooltipProps}
+                        placement="right" arrow>
+                        <span>{depletionDateString}</span>
+                    </Tooltip>
+                </Box>
+            </>
+        );
+    }
+
+    return null;
 };
 
 export default DepletionDate;
