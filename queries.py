e1ea60f246d608d2dad5ea6a9436c43a63b22654
rodriguezmatid <matias.demian.r@gmail.com>
2024-05-21 00:26:20 -0300

    queries

--- queries.py
+++ queries.py
@@ -5,33 +5,15 @@ def supertoken_metrics(token_address):
     {
         tokenStatistics(where: {token: "$token_address"}) {
             id
-            totalDeposit
             totalNumberOfAccounts
-            totalNumberOfActiveIndexes
             totalNumberOfActivePools
             totalNumberOfActiveStreams
-            totalNumberOfClosedStreams
             totalNumberOfHolders
-            totalNumberOfIndexes
-            totalNumberOfPools
             totalOutflowRate
-            totalSubscriptionsWithUnits
             totalSupply
-            totalMembershipsWithUnits
-            totalGDAOutflowRate
-            totalGDANumberOfClosedStreams
-            totalGDANumberOfActiveStreams
-            totalGDADeposit
-            totalCFAOutflowRate
             totalConnectedMemberships
-            totalCFANumberOfClosedStreams
-            totalCFANumberOfActiveStreams
-            totalCFADeposit
-            totalApprovedSubscriptions
-            totalAmountTransferredUntilUpdatedAt
             totalAmountStreamedUntilUpdatedAt
             totalAmountDistributedUntilUpdatedAt
-            totalCFAAmountStreamedUntilUpdatedAt
         }
     }
     ''').substitute(token_address=token_address)
