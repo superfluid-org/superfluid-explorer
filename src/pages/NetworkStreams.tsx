22129ad82cc05953d40d9003d493dc42e1cffde8
Juan <jdeluca@itba.edu.ar>
2024-06-19 21:39:10 +0200

    changed latest streams view

--- src/pages/NetworkStreams.tsx
+++ src/pages/NetworkStreams.tsx
@@ -6,6 +6,8 @@ import {
   TableCell,
   TableFooter,
   TableRow,
+  ToggleButton,
+  ToggleButtonGroup,
   Typography
 } from '@mui/material'
 import {
@@ -14,7 +16,7 @@ import {
   SkipPaging,
   Stream_OrderBy
 } from '@superfluid-finance/sdk-core'
-import React, { FC, useState } from 'react'
+import React, { FC, useCallback, useEffect, useState } from 'react'
 
 import AccountAddress from '../components/Address/AccountAddress'
 import FlowingBalanceWithToken from '../components/Amount/FlowingBalanceWithToken'
@@ -24,6 +26,18 @@ import TimeAgo from '../components/TimeAgo/TimeAgo'
 import { Network } from '../redux/networks'
 import { sfSubgraph } from '../redux/store'
 import SimpleStream from '../components/SimpleStream/SimpleStream'
+import ReactFlow, {
+  OnConnect,
+  Position,
+  addEdge,
+  useEdgesState,
+  useNodesState
+} from 'reactflow'
+
+export enum ViewMode {
+  Table,
+  Map
+}
 
 export const defaultStreamQueryOrdering: Ordering<Stream_OrderBy> = {
   orderBy: 'createdAtTimestamp',
@@ -40,6 +54,10 @@ interface NetworkStreamsProps {
 
 export const NetworkStreams: FC<NetworkStreamsProps> = ({ network }) => {
   const [paging, setPaging] = useState<SkipPaging>(defaultStreamQueryPaging)
+  const [viewMode, setViewMode] = useState<ViewMode>(ViewMode.Table)
+
+  const onViewModeChange = (_event: any, newViewMode: ViewMode) =>
+    setViewMode(newViewMode)
 
   const query = sfSubgraph.useStreamsQuery({
     chainId: network.chainId,
@@ -55,77 +73,154 @@ export const NetworkStreams: FC<NetworkStreamsProps> = ({ network }) => {
 
   const streams = query.data?.data ?? []
 
+  const initialNodes = streams
+    .map((stream, index) => [
+      {
+        id: `${stream.sender}`,
+        type: 'input',
+        position: { x: -200, y: 0 },
+        data: {
+          label: stream.sender.slice(0, 6) + '...' + stream.sender.slice(-4)
+        },
+        sourcePosition: Position.Right,
+        targetPosition: Position.Left
+      },
+      {
+        id: `${stream.receiver}`,
+        position: { x: 150, y: 0 },
+        data: {
+          label: stream.receiver.slice(0, 6) + '...' + stream.receiver.slice(-4)
+        },
+        sourcePosition: Position.Right,
+        targetPosition: Position.Left
+      }
+    ])
+    .flat()
+
+  const initialEdges = streams
+    .map((stream, index) => [
+      {
+        id: `${stream.id}`,
+        source: `${stream.sender}`,
+        target: `${stream.receiver}`,
+        animated: true,
+        sourceHandle: 'left',
+        targetHandle: 'right',
+        type: 'straight'
+      }
+    ])
+    .flat()
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+
+  useEffect(() => {
+    // need to update nodes and edges when streams change
+  }, [paging])
+
+  const onConnect: OnConnect = useCallback(
+    (connection) => setEdges((edges) => addEdge(connection, edges)),
+    [setEdges]
+  )
+
   return (
-    <Table sx={{ tableLayout: 'fixed' }}>
-      <TableBody>
-        {streams.map((stream) => (
-          <React.Fragment key={stream.id}>
-            <TableRow hover sx={{ '& > *': { borderBottom: 'none' } }}>
-              <TableCell width="60%">
-                <SenderReceiver
-                  network={network}
-                  fromAddress={stream.sender}
-                  toAddress={stream.receiver}
-                />
-              </TableCell>
-              <TableCell width="50%">
-                <TotalStreamed
-                  network={network}
-                  tokenAddress={stream.token}
-                  balance={stream.streamedUntilUpdatedAt}
-                  balanceTimestamp={stream.updatedAtTimestamp}
-                  flowRate={stream.currentFlowRate}
-                />
-              </TableCell>
-              <TableCell width="20%" align="right">
-                <TimeAgo
-                  subgraphTime={stream.createdAtTimestamp}
-                  typographyProps={{ typography: 'body2' }}
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell
-                style={{ paddingBottom: 0, paddingTop: 0 }}
-                colSpan={6}
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Box marginBottom="10px" width="50%">
+        <ToggleButtonGroup
+          exclusive
+          fullWidth
+          size="small"
+          value={viewMode}
+          onChange={onViewModeChange}
+        >
+          <ToggleButton value={ViewMode.Table}>Table view</ToggleButton>
+          <ToggleButton value={ViewMode.Map}>Map view</ToggleButton>
+        </ToggleButtonGroup>
+      </Box>
+      <Table sx={{ tableLayout: 'fixed' }}>
+        {viewMode === ViewMode.Table && (
+          <TableBody>
+            {streams.map((stream) => (
+              <TableRow
+                hover
+                sx={{ '& > *': { borderBottom: 'none' } }}
+                key={stream.id}
               >
-                <Box sx={{ margin: 1 }}>
-                  <SimpleStream
-                    id={stream.id}
-                    sender={stream.sender}
-                    receiver={stream.receiver}
-                    token={stream.token}
+                <TableCell width="60%">
+                  <SenderReceiver
+                    network={network}
+                    fromAddress={stream.sender}
+                    toAddress={stream.receiver}
+                  />
+                </TableCell>
+                <TableCell width="50%">
+                  <TotalStreamed
+                    network={network}
+                    tokenAddress={stream.token}
+                    balance={stream.streamedUntilUpdatedAt}
+                    balanceTimestamp={stream.updatedAtTimestamp}
                     flowRate={stream.currentFlowRate}
                   />
-                </Box>
+                </TableCell>
+                <TableCell width="20%" align="right">
+                  <TimeAgo
+                    subgraphTime={stream.createdAtTimestamp}
+                    typographyProps={{ typography: 'body2' }}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+
+            <TableLoader
+              isLoading={query.isLoading || query.isFetching}
+              showSpacer={streams.length === 0}
+              minHeight="576px"
+            />
+          </TableBody>
+        )}
+
+        {viewMode === ViewMode.Map && (
+          <Box>Coming soon!</Box>
+          // <ReactFlow
+          //   style={{
+          //     width: '100%',
+          //     minHeight: '500px',
+          //     backgroundColor: '#303030'
+          //   }}
+          //   nodes={nodes}
+          //   onNodesChange={onNodesChange}
+          //   edges={edges}
+          //   onEdgesChange={onEdgesChange}
+          //   onConnect={onConnect}
+          //   fitView={true}
+          //   minZoom={1.5}
+          //   maxZoom={1.5}
+          //   edgesUpdatable={false}
+          //   nodesDraggable={false}
+          //   nodesConnectable={false}
+          //   panOnDrag={false}
+          //   zoomOnScroll={false}
+          //   nodesFocusable={false}
+          // />
+        )}
+
+        {streams.length > 0 && (
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3} align="right">
+                <InfinitePagination
+                  page={(paging.skip ?? 0) / paging.take + 1}
+                  isLoading={query.isFetching}
+                  hasNext={!!query.data?.nextPaging}
+                  onPageChange={onPageChange}
+                  sx={{ justifyContent: 'flex-end' }}
+                />
               </TableCell>
             </TableRow>
-          </React.Fragment>
-        ))}
-
-        <TableLoader
-          isLoading={query.isLoading || query.isFetching}
-          showSpacer={streams.length === 0}
-          minHeight="576px"
-        />
-      </TableBody>
-
-      {streams.length > 0 && (
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3} align="right">
-              <InfinitePagination
-                page={(paging.skip ?? 0) / paging.take + 1}
-                isLoading={query.isFetching}
-                hasNext={!!query.data?.nextPaging}
-                onPageChange={onPageChange}
-                sx={{ justifyContent: 'flex-end' }}
-              />
-            </TableCell>
-          </TableRow>
-        </TableFooter>
-      )}
-    </Table>
+          </TableFooter>
+        )}
+      </Table>
+    </Box>
   )
 }
 
