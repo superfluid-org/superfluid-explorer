d1ecab8b772cda7f62660804c687b4eb46a73542
Juan <jdeluca@itba.edu.ar>
2024-07-06 16:20:45 +0200

    delete frontend folder

--- frontend/src/services/TokenService.ts
+++
@@ -1,9 +0,0 @@
-import { IToken } from '../types/Token'
-import { ApiService } from './ApiService'
-
-export const TokenService = {
-  getBaseMainnetSuperTokens: async (): Promise<IToken[]> => {
-    const response = await ApiService.getBaseMainnetSuperTokens()
-    return response
-  }
-}
